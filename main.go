fb861bd8fb00d58efba6d75353736f4dcbf1de64
Brice Frisco <39070938+bricefrisco@users.noreply.github.com>
2024-01-03 17:07:38 -0600

    enhancement: simplify logic, remove unnecessary columns

--- main.go
+++ main.go
@@ -61,11 +61,11 @@ func waitForQueryToComplete(svc *athena.Athena, queryExecutionId string) error {
 	}
 }
 
-func runPartitionQuery(svc *athena.Athena, tableName string, dateInfo map[string]string) error {
-	query := fmt.Sprintf("ALTER TABLE %[1]s "+
-		"ADD PARTITION (year='%[2]s', month='%[3]s', day='%[4]s', hour='%[5]s') "+
-		"LOCATION 's3://sod-auctions/data/year=%[2]s/month=%[3]s/day=%[4]s/hour=%[5]s'",
-		tableName, dateInfo["year"], dateInfo["month"], dateInfo["day"], dateInfo["hour"])
+func runPartitionQuery(svc *athena.Athena, dateInfo map[string]string) error {
+	query := fmt.Sprintf("ALTER TABLE sod_auctions "+
+		"ADD PARTITION (year='%[1]s', month='%[2]s', day='%[3]s', hour='%[4]s') "+
+		"LOCATION 's3://sod-auctions/data/year=%[1]s/month=%[2]s/day=%[3]s/hour=%[4]s'",
+		dateInfo["year"], dateInfo["month"], dateInfo["day"], dateInfo["hour"])
 
 	outputLocation := "results/partitioning"
 
@@ -77,9 +77,8 @@ func runPartitionQuery(svc *athena.Athena, tableName string, dateInfo map[string
 	return waitForQueryToComplete(svc, aws.StringValue(output.QueryExecutionId))
 }
 
-func runAggregationQuery(svc *athena.Athena, interval int, dateInfo map[string]string) (*athena.StartQueryExecutionOutput, error) {
-	end, _ := strconv.Atoi(dateInfo["hour"])
-	start := end - (interval - 1)
+func runAggregationQuery(svc *athena.Athena, dateInfo map[string]string) (*athena.StartQueryExecutionOutput, error) {
+	hour, _ := strconv.Atoi(dateInfo["hour"])
 
 	layout := "2006-01-02 15Z"
 	dateStr := fmt.Sprintf("%s-%s-%s %sZ", dateInfo["year"], dateInfo["month"], dateInfo["day"], dateInfo["hour"])
@@ -89,14 +88,8 @@ func runAggregationQuery(svc *athena.Athena, interval int, dateInfo map[string]s
 	}
 
 	timestamp := date.Format(time.RFC3339)
-	year, _ := strconv.Atoi(date.Format("2006"))
-	month, _ := strconv.Atoi(date.Format("01"))
-	day, _ := strconv.Atoi(date.Format("02"))
-	hour, _ := strconv.Atoi(date.Format("15"))
-	dayOfWeek := int(date.Weekday())
 
 	query := fmt.Sprintf("SELECT '%s' AS timestamp, "+
-		"'%d' AS year, '%d' AS month, '%d' AS day, '%d' AS dayOfWeek, '%d' AS hour, "+
 		"realmId, auctionHouseId, "+
 		"itemId, SUM(quantity) AS quantity, "+
 		"MIN(buyoutEach) AS min, MAX(buyoutEach) AS max, APPROX_PERCENTILE(buyoutEach, 0.05) AS p05, "+
@@ -104,12 +97,11 @@ func runAggregationQuery(svc *athena.Athena, interval int, dateInfo map[string]s
 		"APPROX_PERCENTILE(buyoutEach, 0.5) AS p50, APPROX_PERCENTILE(buyoutEach, 0.75) AS p75, "+
 		"APPROX_PERCENTILE(buyoutEach, 0.9) AS p90 "+
 		"FROM sod_auctions "+
-		"WHERE year='%s' AND month='%s' AND day='%s' AND CAST(hour AS integer) BETWEEN %d and %d "+
-		"GROUP BY realmId, auctionHouseId, itemId", timestamp, year, month, day, dayOfWeek, hour,
-		dateInfo["year"], dateInfo["month"], dateInfo["day"], start, end)
+		"WHERE year='%s' AND month='%s' AND day='%s' AND CAST(hour AS integer) = %d "+
+		"GROUP BY realmId, auctionHouseId, itemId", timestamp, dateInfo["year"], dateInfo["month"], dateInfo["day"], hour)
 
-	outputLocation := fmt.Sprintf("results/aggregates/interval=%[1]d/year=%[2]s/month=%[3]s/day=%[4]s/hour=%[5]s",
-		interval, dateInfo["year"], dateInfo["month"], dateInfo["day"], dateInfo["hour"])
+	outputLocation := fmt.Sprintf("results/aggregates/interval=1/year=%s/month=%s/day=%s/hour=%s",
+		dateInfo["year"], dateInfo["month"], dateInfo["day"], dateInfo["hour"])
 
 	return runAthenaQuery(svc, query, outputLocation)
 }
@@ -146,41 +138,16 @@ func handler(ctx context.Context, event events.S3Event) error {
 
 		log.Printf("starting athena queries for file %s\n", key)
 
-		log.Println("running partition query for table sod_auctions")
-		err = runPartitionQuery(svc, "sod_auctions", dateInfo)
+		log.Println("running partition query...")
+		err = runPartitionQuery(svc, dateInfo)
 		if err != nil {
 			return fmt.Errorf("error occurred while running partioning query: %v", err)
 		}
 
-		log.Println("running aggregate query for interval=1..")
-		_, err = runAggregationQuery(svc, 1, dateInfo)
+		log.Println("running aggregate query...")
+		_, err = runAggregationQuery(svc, dateInfo)
 		if err != nil {
 			return fmt.Errorf("error occurred while running distribution query: %v", err)
-		}
-
-		ahour, _ := strconv.Atoi(dateInfo["hour"])
-		if ahour == 5 || ahour == 11 || ahour == 17 || ahour == 23 {
-			log.Println("running aggregate query for interval=6..")
-			_, err = runAggregationQuery(svc, 6, dateInfo)
-			if err != nil {
-				return fmt.Errorf("error occurred while running distribution query: %v", err)
-			}
-		}
-
-		if ahour == 11 || ahour == 23 {
-			log.Println("running aggregate query for interval=12..")
-			_, err = runAggregationQuery(svc, 12, dateInfo)
-			if err != nil {
-				return fmt.Errorf("error occurred while running distribution query: %v", err)
-			}
-		}
-
-		if ahour == 23 {
-			log.Println("running aggregate query for interval=24..")
-			_, err = runAggregationQuery(svc, 24, dateInfo)
-			if err != nil {
-				return fmt.Errorf("error occurred while running distribution query: %v", err)
-			}
 		}
 
 		log.Println("running distribution query..")
